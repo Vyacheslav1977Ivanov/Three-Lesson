2e6554ec3e596b4c5ad93c966569d7eb104d291b
Vyacheslav Ivanov <vya1977@mail.ru>
2023-11-02 20:45:18 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿int[] GenerateArray()
+{
+    int[] array = new int[10];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write($"Сгенерированный массив размерностью {array.Length} элементов : ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int FindingEvenNumbers(int[] arg1)
+{
+   int result = 0;
+   int i = 0;
+  for(i = 0; i < arg1.Length;i++)
+  {
+    if(arg1[i] % 2 == 0 ) 
+    {
+         result++;
+        Console.Write($" Этот Элемент чётный - {arg1[i]} "); 
+    }
+    else
+    {
+        Console.Write($" Этот элемент не четный  - {arg1[i]} ");
+    }
+  }
+  Console.Write($"Четных элементов : {result}");
+  return result;
+}          
+
+int[] mass = GenerateArray();
+PrintArray(mass);
+FindingEvenNumbers(mass);
