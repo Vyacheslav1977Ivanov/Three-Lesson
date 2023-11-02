e9558094acfb46d363a6199fd0d34a99d5cd64fd
Vyacheslav Ivanov <vya1977@mail.ru>
2023-11-02 20:41:18 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] GenerateArray()
+{
+    int[] array = new int[10];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write($"Сгенерированный массив размерностью {array.Length} элементов : ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int KolvoElem(int[] array)
+{
+    int Kolvo = 0;
+    int index = 0;
+    while(index < array.Length)
+    {
+        if(array[index] >= 20 && array[index] <= 90)
+        {
+            Kolvo++;
+        }
+        index++;
+    }
+    return Kolvo;
+}
+int[] mass = GenerateArray();
+PrintArray(mass);
+// int N = KolvoElem(mass);
+Console.Write($"КОЛИЧЕСТВО ЭЛЕМЕНТОВ = {KolvoElem(mass)}");
